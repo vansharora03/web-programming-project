a92eab0e20295f10c548e4e1e8973e9ca795a8c3
JJ H. <jj.huynh00@gmail.com>
2025-04-15 08:59:55 -0400

    Created API folder with routes

---
+++ project/src/app/api/items/route.ts
@@ -0,0 +1,8 @@
+import connectMongoDB from "../../../../config/mongodb";
+// Recipe Schema
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+// POST
+ 
+// GET 
