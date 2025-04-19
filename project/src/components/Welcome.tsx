cbe55f877fd670f6e6954dd80ac7189425707e0a
Montana-Jones <mgj58820@uga.edu>
2025-04-19 17:32:28 -0400

    Updated description in Welcome.tsx

--- project/src/components/Welcome.tsx
+++ project/src/components/Welcome.tsx
@@ -12,8 +12,8 @@ const Welcome = () => {
           Byte Sized is developed to help users add nutrition to their diet
           rather than restrict foods. With its vast search engine, users are
           able to find any recipe to their liking filled with the needed
-          nutrients and then the app will scan of all the ingredients to have a
-          fully-loaded grocery list for the user.
+          nutrients and save them to their favorites so they can try new foods
+          or simply learn how to cook.
         </p>
       </div>
       <div className="grid justify-center">
@@ -25,9 +25,10 @@ const Welcome = () => {
           browse hundreds of recipes on the recipes page. If you find something
           you like, add it to your favorites list so you can try it! Looking for
           something a little more specific? Use the search bar to narrow down
-          your search based on calorie goals, cuisine, or your diet. Once you've
-          found what you are looking for, navigate to your favorites page to see
-          all the recipes you've saved.
+          your search based on types of dishes (soups, salads, desserts, etc).
+          Once you've found what you are looking for, navigate to your favorites
+          page to see all the recipes you've saved so you can make your grocery
+          list more than just instant noodles.
         </p>
       </div>
     </div>
